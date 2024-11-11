aa6ed3735a172ec21c27797478a62c0b9aa90836
pyoseung74 <132826720+pyoseung74@users.noreply.github.com>
2024-11-11 16:04:20 +0900

    Update datastruct.c

--- code/datastruct.c
+++ code/datastruct.c
@@ -6,42 +6,55 @@ typedef struct
 	char data[200];
 	int front;
 	int rear;
-}queue;
+} queue;
 
 queue CreateQ();
 void EnQ(char a, queue* Q);
 char DeQ(queue* Q);
-char Peek(queue* Q);
+char Peek(queue Q);
 int IsEmpty(queue Q);
 
-int main() 
+int main()
 {
-	// 계산기 작성 과제 
-	// 값 입력 받기로 바꾸기
-	// 뺄셈기능 추가하기
-	queue NQ = CreateQ();
-	char equation[100] = "5759+787+2124+575";
+    queue NQ = CreateQ();
+    char equation[100] = "5759+787-2124+575";
 
-	for (int i = 0; i < strlen(equation); i++)
-	{
-		EnQ(equation[i], &NQ);
-	}
+    for (int i = 0; i < strlen(equation); i++)
+    {
+        EnQ(equation[i], &NQ);
+    }
 
-	int num = 0;
-	int sum = 0;
-	while (IsEmpty(NQ)==0)
-	{
-		char temp = DeQ(&NQ);
-		if(temp >= '0' && temp <= '9')
-			num = num * 10 + (temp - 32);
-		else
-		{
-			sum += num;
-			num = 0;
-		}
-		printf("%d\n", sum);
-	}
-	return 0;
+    int num = 0;
+    int sum = 0;
+    char operation = '+';
+
+    while (IsEmpty(NQ) == 0)
+    {
+        char temp = DeQ(&NQ);
+        
+        if (temp >= '0' && temp <= '9')
+        {
+            num = num * 10 + (temp - '0');
+        }
+        else
+        {
+            if (operation == '+')
+                sum += num;
+            else if (operation == '-')
+                sum -= num;
+
+            operation = temp;
+            num = 0;
+        }
+    }
+    
+    if (operation == '+')
+        sum += num;
+    else if (operation == '-')
+        sum -= num;
+
+    printf("Result: %d\n", sum);
+    return 0;
 }
 
 queue CreateQ()
@@ -49,8 +62,7 @@ queue CreateQ()
 	queue newQ;
 	newQ.front = -1;
 	newQ.rear = -1;
-	newQ.data[0] = 0;
-
+	return newQ;
 }
 
 void EnQ(char a, queue* Q)
@@ -60,10 +72,10 @@ void EnQ(char a, queue* Q)
 
 char DeQ(queue* Q)
 {
-	if (!IsEmpty(*Q))
-		return -1;
-	else
+	if (IsEmpty(*Q))
 		return Q->data[++(Q->front)];
+	else
+		return -1;
 }
 
 char Peek(queue Q)
@@ -71,5 +83,10 @@ char Peek(queue Q)
 	if (IsEmpty(Q))
 		return -1;
 	else
-		return Q.data[++(Q.front + 1)];
+		return Q.data[Q.front + 1];
+}
+
+int IsEmpty(queue Q)
+{
+	return Q.front == Q.rear;
 }
